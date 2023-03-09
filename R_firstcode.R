6d12becce11e025bce305554943124778731fd1e
alessandravalentimartinez <126901360+alessandravalentimartinez@users.noreply.github.com>
2023-03-09 17:40:36 +0100

    Create R_firstcode.R

---
+++ R_firstcode.R
@@ -0,0 +1,7 @@
+#My first code in Git Hub
+#raster ci indica tutti gli indici di diversità per matrici numeriche
+
+#i pacchetti si trovano nel CRAN, è il server dal quale abbiamo scaricato anche R. Andiamo nel CRAN per scaricare un pacchetto
+install.packages("raster")
+library(raster)
+
