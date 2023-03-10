1c31beb48bc3e3f7eebf90b5e4a8f303c50a18ef
alessandravalentimartinez <126901360+alessandravalentimartinez@users.noreply.github.com>
2023-03-10 17:56:32 +0100

    Update R_firstcode.R

--- R_firstcode.R
+++ R_firstcode.R
@@ -5,3 +5,34 @@
 install.packages("raster")
 library(raster)
 
+#import data, setting the working directory
+setwd("C:/lab")
+getwd()
+
+#importiamo il dato e lo assegno ad una variabile
+l2011 <- brick("p224r63_2011_masked.grd")
+
+#plottiamo i data
+plot(l2011)
+
+#vediamo la riflettanza nella banda 1, in landsat era il blu. #banda 4, near infrared.
+#cambiamo i colori
+
+cl<- colorRampPalette(c("red", "orange", "yellow")) (100)
+
+#fuori mettiamo il numero delle sfumature che vogliamo 
+
+plot(l2011, col=cl)
+#abbiamo rifatto il plot come prima ma il colore è nuovo, con cl
+#abbiamo la possibilità di utilizzare una banda sola, prendiamo un elemento
+#plottiamo il quarto elemento, con l'infrarosso
+
+plot(l2011[[4]], col=cl)
+
+#posso utilizzare anche il nome dell'elemento, leghiamo i due pezzi con il dollaro
+l2011
+plot(l2011$B4_sre, col=cl)
+
+nir <- l2011[[4]] #or : nir <- l2011$B4_sre
+
+plot(nir, col=cl)
