fd10cebe29449ed7402bf632af6ec82d752ce4ed
alessandravalentimartinez <126901360+alessandravalentimartinez@users.noreply.github.com>
2024-02-21 14:05:46 +0100

    Update R_code_exam.R

--- R_code_exam.R
+++ R_code_exam.R
@@ -134,6 +134,25 @@ ndvistacked <- stack(import)
 lapalma_ndvistacked <- crop(ndvistacked, new_ext)
 lapalma_ndvistacked
 
+
+# Now let's look at the difference between the NDVI in 2021 and in 2022
+ndvidifference2122_raw <- lapalma_ndvistacked[[3]]-lapalma_ndvistacked[[2]]
+
+# Set all values that equal zero to NA
+ndvidifference2122_ <- ndvidifference2122_raw; ndvidifference2122_[ndvidifference2122_ == 0] <- NA 
+ndvidifference2122 <- as.data.frame(ndvidifference2122_, xy=TRUE)
+
+#Let's plot the difference
+ndvidifference2122_plot <- ggplot() + 
+  geom_raster(ndvidifference2122, mapping = aes(x=x,  y = y, fill = layer)) +
+  scale_fill_viridis(option = "rocket") +
+  ggtitle("NDVI difference between 2021 2022")
+
+ndvidifference2122_plot
+
+ggsave(filename = "NDVI_difference2122.png", plot = ndvidifference2122_plot)
+
+
 # Now let's look at the difference between the NDVI in 2020 and in 2023
 
 ndvidifference2023_raw <- lapalma_ndvistacked[[4]]-lapalma_ndvistacked[[1]]
@@ -152,23 +171,6 @@ ndvidifference2023_plot
 
 ggsave(filename = "NDVI_difference2023.png", plot = ndvidifference2023_plot)
 
-
-# Now let's look at the difference between the NDVI in 2021 and in 2022
-ndvidifference2122_raw <- lapalma_ndvistacked[[3]]-lapalma_ndvistacked[[2]]
-
-# Set all values that equal zero to NA
-ndvidifference2122_ <- ndvidifference2122_raw; ndvidifference2122_[ndvidifference2122_ == 0] <- NA 
-ndvidifference2122 <- as.data.frame(ndvidifference2122_, xy=TRUE)
-
-#Let's plot the difference
-ndvidifference2122_plot <- ggplot() + 
-  geom_raster(ndvidifference2122, mapping = aes(x=x,  y = y, fill = layer)) +
-  scale_fill_viridis(option = "rocket") +
-  ggtitle("NDVI difference between 2021 2022")
-
-ndvidifference2122_plot
-
-ggsave(filename = "NDVI_difference2122.png", plot = ndvidifference2122_plot)
 
 
 
@@ -285,20 +287,6 @@ fcoverstacked_newext <- crop(fcoverstacked, new_ext)
 fcoverstacked_newext
 
 
-#Now let's look at the difference between 2023 and 2020
-fcover_difference2320 <- fcoverstacked_newext[[4]]-fcoverstacked_newext[[1]]
-fcover_difference2320_df <- as.data.frame(fcover_difference2320, xy = TRUE)
-
-fcoverdifference2320_plot <- ggplot() + 
-  geom_raster(fcover_difference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
-  scale_fill_viridis(option = "magma") +
-  ggtitle("Difference in Fraction of Green Vegetation Cover 2020 2023") 
-
-fcoverdifference2320_plot
-
-ggsave(filename = "FCOVER_difference2320.png", plot = fcoverdifference2320_plot)
-
-
 #Now let's look at the difference between 2021 and 2022
 fcover_difference2122 <- fcoverstacked_newext[[3]]-fcoverstacked_newext[[2]]
 fcover_difference2122_df <- as.data.frame(fcover_difference2122, xy = TRUE)
@@ -306,11 +294,26 @@ fcover_difference2122_df <- as.data.frame(fcover_difference2122, xy = TRUE)
 fcoverdifference2122_plot <- ggplot() + 
   geom_raster(fcover_difference2122_df, mapping = aes(x=x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "magma") +
-  ggtitle("Difference in Fraction of Green Vegetation Cover 2021 2022") 
+  ggtitle("Difference in Fcover 2021 2022") 
 
 fcoverdifference2122_plot
 
 ggsave(filename = "FCOVER_difference2122.png", plot = fcoverdifference2122_plot)
+
+
+
+#Now let's look at the difference between 2023 and 2020
+fcover_difference2320 <- fcoverstacked_newext[[4]]-fcoverstacked_newext[[1]]
+fcover_difference2320_df <- as.data.frame(fcover_difference2320, xy = TRUE)
+
+fcoverdifference2320_plot <- ggplot() + 
+  geom_raster(fcover_difference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  scale_fill_viridis(option = "magma") +
+  ggtitle("Difference in Fcover 2020 2023") 
+
+fcoverdifference2320_plot
+
+ggsave(filename = "FCOVER_difference2320.png", plot = fcoverdifference2320_plot)
 
 
 
@@ -421,19 +424,6 @@ laistacked_newext <- crop(laistacked, new_ext)
 laistacked_newext
 
 
-# Now let's look at the difference between 2023 and 2020
-lai_difference2320 <- laistacked_newext[[4]]-laistacked_newext[[1]]
-laidifference2320_df <-  as.data.frame(lai_difference2320, xy = TRUE)
-
-laidifference2320_plot <- ggplot() + 
-  geom_raster(laidifference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
-  scale_fill_viridis(option = "rocket") +
-  ggtitle("LAI difference 2020 2023")
-
-laidifference2320_plot
-ggsave(filename = "LAI_difference2320.png", plot = laidifference2320_plot)
-
-
 # Now let's look at the difference between 2021 and 2022
 lai_difference2122 <- laistacked_newext[[3]]-laistacked_newext[[2]]
 laidifference2122_df <-  as.data.frame(lai_difference2122, xy = TRUE)
@@ -445,3 +435,16 @@ laidifference2122_plot <- ggplot() +
 
 laidifference2122_plot
 ggsave(filename = "LAI_difference2122.png", plot = laidifference2122_plot)
+
+
+# Now let's look at the difference between 2023 and 2020
+lai_difference2320 <- laistacked_newext[[4]]-laistacked_newext[[1]]
+laidifference2320_df <-  as.data.frame(lai_difference2320, xy = TRUE)
+
+laidifference2320_plot <- ggplot() + 
+  geom_raster(laidifference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  scale_fill_viridis(option = "rocket") +
+  ggtitle("LAI difference 2020 2023")
+
+laidifference2320_plot
+ggsave(filename = "LAI_difference2320.png", plot = laidifference2320_plot)
