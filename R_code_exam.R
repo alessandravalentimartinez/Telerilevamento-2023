7275913a38b0cd30825cebefa9e4dafeb512d3dc
alessandravalentimartinez <126901360+alessandravalentimartinez@users.noreply.github.com>
2024-02-21 14:20:11 +0100

    Update R_code_exam.R

--- R_code_exam.R
+++ R_code_exam.R
@@ -53,13 +53,13 @@ ndvi2023_newext
 
 ##2020
 ndvi2020_gg <- ggplot() + 
-  geom_raster(ndvi2020_newext, mapping = aes(x=x,  y = y, fill = NDVI)) +
+  geom_raster(ndvi2020_newext, mapping = aes(x = x,  y = y, fill = NDVI)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2020 NDVI")
 
 
 #Let's add the legend
-ndvi2020_gg <- ndvi2020_gg + labs(fill= "NDVI")
+ndvi2020_gg <- ndvi2020_gg + labs(fill = "NDVI")
 ndvi2020_gg
 
 #Let's save the new plot
@@ -68,13 +68,13 @@ ggsave(filename = "ndvi2020.png", plot = ndvi2020_gg)
 
 ##2021
 ndvi2021_gg <- ggplot() + 
-  geom_raster(ndvi2021_newext, mapping = aes(x=x,  y = y, fill = NDVI)) +
+  geom_raster(ndvi2021_newext, mapping = aes(x = x,  y = y, fill = NDVI)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2021 NDVI")
 
 
 #Let's add the legend
-ndvi2021_gg <- ndvi2021_gg + labs(fill= "NDVI")
+ndvi2021_gg <- ndvi2021_gg + labs(fill = "NDVI")
 ndvi2021_gg
 
 #Let's save the new plot
@@ -82,13 +82,13 @@ ggsave(filename = "ndvi2021.png", plot = ndvi2021_gg)
 
 ##2022
 ndvi2022_gg <- ggplot() + 
-  geom_raster(ndvi2022_newext, mapping = aes(x=x,  y = y, fill = NDVI)) +
+  geom_raster(ndvi2022_newext, mapping = aes(x = x,  y = y, fill = NDVI)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2022 NDVI")
 
 
 #Let's add the legend
-ndvi2022_gg <- ndvi2022_gg + labs(fill= "NDVI")
+ndvi2022_gg <- ndvi2022_gg + labs(fill = "NDVI")
 ndvi2022_gg
 
 #Let's save the new plot
@@ -97,22 +97,25 @@ ggsave(filename = "ndvi2022.png", plot = ndvi2022_gg)
 
 ##2023
 ndvi2023_gg <- ggplot() + 
-  geom_raster(ndvi2023_newext, mapping = aes(x=x,  y = y, fill = NDVI)) +
+  geom_raster(ndvi2023_newext, mapping = aes(x = x,  y = y, fill = NDVI)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2023 NDVI")
 
 
 #Let's add the legend
-ndvi2023_gg <- ndvi2023_gg + labs(fill= "NDVI")
+ndvi2023_gg <- ndvi2023_gg + labs(fill = "NDVI")
 ndvi2023_gg
 
 #Let's save the new plot
 ggsave(filename = "ndvi2023.png", plot = ndvi2023_gg)
 
 
+
+
 # Now let's look at all the plots together to compare them
 NDVIcomparison <- ndvi2020_gg + ndvi2021_gg + ndvi2022_gg + ndvi2023_gg
-print(NDVIcomparison)
+NDVIcomparison
+
 ggsave(filename = "NDVIcomparison.png" , plot = NDVIcomparison)
 
 
@@ -120,31 +123,32 @@ ggsave(filename = "NDVIcomparison.png" , plot = NDVIcomparison)
 ########
 
 #List of files
-rlist <- list.files(pattern ="ndvi") #make a list of the files that start with "ndvi"
+rlist <- list.files(pattern = "ndvi") #make a list of the files that start with "ndvi"
 rlist 
 
 
 # Apply a function over a list or vector
-import <- lapply(rlist, raster) # specify the list and the function to import the data
+import <- lapply(rlist, raster) 
 import 
 
 # Let's stack the data, all the four layers in a single file:
 ndvistacked <- stack(import)
 
+#Let's crop the data
 lapalma_ndvistacked <- crop(ndvistacked, new_ext)
 lapalma_ndvistacked
 
 
 # Now let's look at the difference between the NDVI in 2021 and in 2022
-ndvidifference2122_raw <- lapalma_ndvistacked[[3]]-lapalma_ndvistacked[[2]]
+ndvidifference2122_raw <- lapalma_ndvistacked[[3]] - lapalma_ndvistacked[[2]]
 
 # Set all values that equal zero to NA
 ndvidifference2122_ <- ndvidifference2122_raw; ndvidifference2122_[ndvidifference2122_ == 0] <- NA 
-ndvidifference2122 <- as.data.frame(ndvidifference2122_, xy=TRUE)
+ndvidifference2122 <- as.data.frame(ndvidifference2122_, xy = TRUE)
 
 #Let's plot the difference
 ndvidifference2122_plot <- ggplot() + 
-  geom_raster(ndvidifference2122, mapping = aes(x=x,  y = y, fill = layer)) +
+  geom_raster(ndvidifference2122, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "rocket") +
   ggtitle("NDVI difference between 2021 2022")
 
@@ -154,16 +158,15 @@ ggsave(filename = "NDVI_difference2122.png", plot = ndvidifference2122_plot)
 
 
 # Now let's look at the difference between the NDVI in 2020 and in 2023
-
-ndvidifference2023_raw <- lapalma_ndvistacked[[4]]-lapalma_ndvistacked[[1]]
+ndvidifference2023_raw <- lapalma_ndvistacked[[4]] - lapalma_ndvistacked[[1]]
 
 # Set all values that equal zero to NA
 ndvidifference2023_ <- ndvidifference2023_raw; ndvidifference2023_[ndvidifference2023_ == 0] <- NA 
-ndvidifference2023 <- as.data.frame(ndvidifference2023_, xy=TRUE)
+ndvidifference2023 <- as.data.frame(ndvidifference2023_, xy = TRUE)
 
 
-ndvidifference2023_plot <-ggplot() + 
-  geom_raster(ndvidifference2023, mapping = aes(x=x,  y = y, fill = layer)) +
+ndvidifference2023_plot <- ggplot() + 
+  geom_raster(ndvidifference2023, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "rocket") +
   ggtitle("NDVI difference between 2020 2023")
 
@@ -203,13 +206,13 @@ fcover2023_newext <- crop(fcover2023, new_ext)
 
 ##2020
 fcover2020_gg <- ggplot() + 
-  geom_raster(fcover2020_newext, mapping = aes(x=x,  y = y, fill = FCOVER)) +
+  geom_raster(fcover2020_newext, mapping = aes(x = x,  y = y, fill = FCOVER)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2020 fcover")
 
 
 #Let's add the legend
-fcover2020_gg <- fcover2020_gg + labs(fill= "fcover")
+fcover2020_gg <- fcover2020_gg + labs(fill = "fcover")
 fcover2020_gg
 
 #Let's save the new plot
@@ -218,12 +221,12 @@ ggsave(filename = "FCOVER2020.png", plot = fcover2020_gg)
 
 ##2021
 fcover2021_gg <- ggplot() + 
-  geom_raster(fcover2021_newext, mapping = aes(x=x,  y = y, fill = FCOVER)) +
+  geom_raster(fcover2021_newext, mapping = aes(x = x,  y = y, fill = FCOVER)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2021 fcover") 
 
 #Let's add the legend
-fcover2021_gg <- fcover2021_gg + labs(fill= "fcover")
+fcover2021_gg <- fcover2021_gg + labs(fill = "fcover")
 fcover2021_gg
 
 #Let's save the new plot
@@ -231,12 +234,12 @@ ggsave(filename = "FCOVER2021.png", plot = fcover2021_gg)
 
 ##2022
 fcover2022_gg <- ggplot() + 
-  geom_raster(fcover2022_newext, mapping = aes(x=x,  y = y, fill = FCOVER)) +
+  geom_raster(fcover2022_newext, mapping = aes(x = x,  y = y, fill = FCOVER)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2022 fcover")
 
 #Let's add the legend
-fcover2022_gg <- fcover2022_gg + labs(fill= "fcover")
+fcover2022_gg <- fcover2022_gg + labs(fill = "fcover")
 fcover2022_gg
 
 #Let's save the new plot
@@ -244,12 +247,12 @@ ggsave(filename = "FCOVER2022.png", plot = fcover2022_gg)
 
 ##2023
 fcover2023_gg <- ggplot() + 
-  geom_raster(fcover2023_newext, mapping = aes(x=x,  y = y, fill = FCOVER)) +
+  geom_raster(fcover2023_newext, mapping = aes(x = x,  y = y, fill = FCOVER)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2023 fcover")
 
 #Let's add the legend
-fcover2023_gg <- fcover2023_gg + labs(fill= "fcover")
+fcover2023_gg <- fcover2023_gg + labs(fill = "fcover")
 fcover2023_gg
 
 #Let's save the new plot
@@ -270,7 +273,7 @@ ggsave(filename = "FCOVER_comparison.png", plot = fcover_comparison)
 ########
 
 # List of files
-rlist <- list.files(pattern= "fcover")
+rlist <- list.files(pattern = "fcover")
 rlist 
 
 # Apply a function over a list or vector
@@ -288,11 +291,12 @@ fcoverstacked_newext
 
 
 #Now let's look at the difference between 2021 and 2022
-fcover_difference2122 <- fcoverstacked_newext[[3]]-fcoverstacked_newext[[2]]
+fcover_difference2122 <- fcoverstacked_newext[[3]] - fcoverstacked_newext[[2]]
 fcover_difference2122_df <- as.data.frame(fcover_difference2122, xy = TRUE)
 
+#Let's plot the difference
 fcoverdifference2122_plot <- ggplot() + 
-  geom_raster(fcover_difference2122_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  geom_raster(fcover_difference2122_df, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("Difference in Fcover 2021 2022") 
 
@@ -303,11 +307,11 @@ ggsave(filename = "FCOVER_difference2122.png", plot = fcoverdifference2122_plot)
 
 
 #Now let's look at the difference between 2023 and 2020
-fcover_difference2320 <- fcoverstacked_newext[[4]]-fcoverstacked_newext[[1]]
+fcover_difference2320 <- fcoverstacked_newext[[4]] - fcoverstacked_newext[[1]]
 fcover_difference2320_df <- as.data.frame(fcover_difference2320, xy = TRUE)
 
 fcoverdifference2320_plot <- ggplot() + 
-  geom_raster(fcover_difference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  geom_raster(fcover_difference2320_df, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "magma") +
   ggtitle("Difference in Fcover 2020 2023") 
 
@@ -342,13 +346,13 @@ lai_2023_newext <- crop(lai_2023, new_ext)
 
 ##2020
 lai_2020_gg <- ggplot() + 
-  geom_raster(lai_2020_newext, mapping = aes(x=x,  y = y, fill = LAI )) +
+  geom_raster(lai_2020_newext, mapping = aes(x = x,  y = y, fill = LAI )) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2020 LAI") 
 
 
 # Let's add the legend
-lai_2020_gg <- lai_2020_gg + labs(fill= "LAI")
+lai_2020_gg <- lai_2020_gg + labs(fill = "LAI")
 lai_2020_gg
 
 # Let's save the new plot
@@ -357,12 +361,12 @@ ggsave(filename = "LAI_2020.png", plot = lai_2020_gg)
 
 ##2021
 lai_2021_gg <- ggplot() + 
-  geom_raster(lai_2021_newext, mapping = aes(x=x,  y = y, fill = LAI )) +
+  geom_raster(lai_2021_newext, mapping = aes(x = x,  y = y, fill = LAI )) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2021 LAI") 
 
 # Let's add the legend
-lai_2021_gg <- lai_2021_gg + labs(fill= "LAI")
+lai_2021_gg <- lai_2021_gg + labs(fill = "LAI")
 lai_2021_gg
 
 # Let's save the new plot
@@ -370,12 +374,12 @@ ggsave(filename = "LAI_2021.png", plot = lai_2021_gg)
 
 ##2022
 lai_2022_gg <- ggplot() + 
-  geom_raster(lai_2022_newext, mapping = aes(x=x,  y = y, fill = LAI )) +
+  geom_raster(lai_2022_newext, mapping = aes(x = x,  y = y, fill = LAI )) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2022 LAI") 
 
 # Let's add the legend
-lai_2022_gg <- lai_2022_gg + labs(fill= "LAI")
+lai_2022_gg <- lai_2022_gg + labs(fill = "LAI")
 lai_2022_gg
 
 # Let's save the new plot
@@ -383,12 +387,12 @@ ggsave(filename = "LAI_2022.png", plot = lai_2022_gg)
 
 #2023
 lai_2023_gg <- ggplot() + 
-  geom_raster(lai_2023_newext, mapping = aes(x=x,  y = y, fill = LAI )) +
+  geom_raster(lai_2023_newext, mapping = aes(x = x,  y = y, fill = LAI )) +
   scale_fill_viridis(option = "magma") +
   ggtitle("2023 LAI") 
 
 # Let's add the legend
-lai_2023_gg <- lai_2023_gg + labs(fill= "LAI")
+lai_2023_gg <- lai_2023_gg + labs(fill = "LAI")
 lai_2023_gg
 
 # Let's save the new plot
@@ -408,7 +412,7 @@ ggsave(filename = "LAI_comparison.png", plot = lai_comparison)
 
 ########
 # List of files
-rlist <- list.files(pattern= "lai_2")
+rlist <- list.files(pattern = "lai_2")
 rlist 
 
 # Apply a function over a list or vector
@@ -425,11 +429,11 @@ laistacked_newext
 
 
 # Now let's look at the difference between 2021 and 2022
-lai_difference2122 <- laistacked_newext[[3]]-laistacked_newext[[2]]
-laidifference2122_df <-  as.data.frame(lai_difference2122, xy = TRUE)
+lai_difference2122 <- laistacked_newext[[3]] - laistacked_newext[[2]]
+laidifference2122_df <- as.data.frame(lai_difference2122, xy = TRUE)
 
 laidifference2122_plot <- ggplot() + 
-  geom_raster(laidifference2122_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  geom_raster(laidifference2122_df, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "rocket") +
   ggtitle("LAI difference 2021 2022")
 
@@ -438,13 +442,14 @@ ggsave(filename = "LAI_difference2122.png", plot = laidifference2122_plot)
 
 
 # Now let's look at the difference between 2023 and 2020
-lai_difference2320 <- laistacked_newext[[4]]-laistacked_newext[[1]]
-laidifference2320_df <-  as.data.frame(lai_difference2320, xy = TRUE)
+lai_difference2320 <- laistacked_newext[[4]] - laistacked_newext[[1]]
+laidifference2320_df <- as.data.frame(lai_difference2320, xy = TRUE)
 
 laidifference2320_plot <- ggplot() + 
-  geom_raster(laidifference2320_df, mapping = aes(x=x,  y = y, fill = layer)) +
+  geom_raster(laidifference2320_df, mapping = aes(x = x,  y = y, fill = layer)) +
   scale_fill_viridis(option = "rocket") +
   ggtitle("LAI difference 2020 2023")
 
 laidifference2320_plot
 ggsave(filename = "LAI_difference2320.png", plot = laidifference2320_plot)
+
